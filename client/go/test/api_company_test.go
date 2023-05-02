77cf14b7352edaf6324793afd25a4840336232b2
sam8helloworld <samcreate0830@gmail.com>
2023-05-03 01:05:43 +0900

    chore: go mod tidy

--- client/go/test/api_company_test.go
+++ client/go/test/api_company_test.go
@@ -11,10 +11,11 @@ package openapi
 
 import (
 	"context"
+	"testing"
+
+	openapiclient "github.com/GIT_USER_ID/GIT_REPO_ID"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"testing"
-	openapiclient "github.com/GIT_USER_ID/GIT_REPO_ID"
 )
 
 func Test_openapi_CompanyApiService(t *testing.T) {
@@ -24,7 +25,7 @@ func Test_openapi_CompanyApiService(t *testing.T) {
 
 	t.Run("Test CompanyApiService GetCompany", func(t *testing.T) {
 
-		t.Skip("skip test")  // remove to run test
+		t.Skip("skip test") // remove to run test
 
 		resp, httpRes, err := apiClient.CompanyApi.GetCompany(context.Background()).Execute()
 
