773c8f4e31a99c213afe5d8947b89d8dc81acd24
sam8helloworld <samcreate0830@gmail.com>
2023-05-03 15:50:44 +0900

    test: GetDailyWorking

--- client/go/test/api_attendance_test.go
+++ client/go/test/api_attendance_test.go
@@ -44,4 +44,14 @@ func Test_openapi_AttendanceApiService(t *testing.T) {
 		assert.Equal(t, 200, httpRes.StatusCode)
 	})
 
+	t.Run("Test AttendanceApiService GetDailyWorking", func(t *testing.T) {
+		date := "2016-05-01"
+		bearer := "8j9f7v4893y58rvt7nyfq2893n75tr78937n83"
+		ctx := context.WithValue(context.Background(), openapiclient.ContextAccessToken, bearer)
+		resp, httpRes, err := apiClient.AttendanceApi.GetDailyWorking(ctx, date).Division("1000").Ondivision(true).AdditionalFields([]string{"currentDateEmployee"}).Execute()
+
+		require.Nil(t, err)
+		require.NotNil(t, resp)
+		assert.Equal(t, 200, httpRes.StatusCode)
+	})
 }
