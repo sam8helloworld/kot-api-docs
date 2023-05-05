884191572e3351e0a468455bdc8111236d3a602a
sam8helloworld <samcreate0830@gmail.com>
2023-05-05 16:20:35 +0900

    refactor: resposne interface

--- client/go/kotclient/kotclient.gen_test.go
+++ client/go/kotclient/kotclient.gen_test.go
@@ -36,3 +36,31 @@ func TestRefreshAccessToken(t *testing.T) {
 		t.Errorf("value mismatch (-want +got):\n%s", diff)
 	}
 }
+
+func TestGetAccessTokenAvailability(t *testing.T) {
+	bearerTokenProvider, err := securityprovider.NewSecurityProviderBearerToken("8j9f7v4893y58rvt7nyfq2893n75tr78937n83")
+	if err != nil {
+		t.Fatalf("failed to securityprovider.NewSecurityProviderBearerToken: %v", err)
+	}
+	ctx := context.Background()
+	sut, err := kotclient.NewClientWithResponses("http://localhost:8001", kotclient.WithRequestEditorFn(bearerTokenProvider.Intercept))
+	if err != nil {
+		t.Fatalf("failed to kotclient.NewClient: %v", err)
+	}
+	input := "8j9f7v4893y58rvt7nyfq2893n75tr78937n83"
+	got, err := sut.GetAccessTokenAvailabilityWithResponse(ctx, input)
+	if err != nil {
+		t.Fatalf("failed to sut.GetAccessTokenAvailabilityWithResponse: %v", err)
+	}
+
+	if diff := cmp.Diff(200, got.StatusCode()); diff != "" {
+		t.Errorf("value is mismatch (-want +got):\n%s", diff)
+	}
+
+	want := kotclient.GetAccessTokenAvailability{
+		Available: true,
+	}
+	if diff := cmp.Diff(kotclient.Ptr(want), got.JSON200); diff != "" {
+		t.Errorf("value mismatch (-want +got):\n%s", diff)
+	}
+}
